ea4225550640651dafb77a85bea18b71b04ee8db
Manoela Urias <maanoelaurias@gmail.com>
2025-01-23 17:36:58 -0300

    atualização do codigo

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class CustomCard extends StatelessWidget {
   final String title;
   final String subtitle;
+  final String imagePath;
 
   const CustomCard({
     super.key,
     required this.title,
     required this.subtitle,
+    required this.imagePath,
   });
 
   @override
@@ -20,17 +22,48 @@ class CustomCard extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
+          // Exibindo a imagem no topo do card
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(12.0),
+              topRight: Radius.circular(12.0),
+            ),
+            child: Image.network(
+              imagePath,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 150,
+            ),
+          ),
           ListTile(
-            title: Text(title),
-            subtitle: Text(subtitle),
+            title: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              subtitle,
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               const SizedBox(width: 8),
               TextButton(
-                child: const Text('Tenho interesse'),
-                onPressed: () {/* ... */},
+                child: const Text(
+                  'Tenho interesse',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  // Função de direcionamento
+                },
               ),
               const SizedBox(width: 8),
             ],
--- lib/screens/loja_screen.dart
+++ lib/screens/loja_screen.dart
@@ -10,6 +10,7 @@ class LojaScreen extends StatelessWidget {
       backgroundColor: Colors.black, // Fundo preto
       body: Center(
         child: CustomCard(
+          imagePath: 'assets/imagens/equipamentoUsado1.jpg',
           title: 'Nome do equipamento (Semi novo)',
           subtitle: 'R\$ 100,00',
         ),
